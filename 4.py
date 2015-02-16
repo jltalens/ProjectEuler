0c720d3f302f1e637a8f943c9aaba667436fa2a6
Jose Talens <jose.talens@williamhill.com>
2015-02-16 13:17:28 +0000

    Found solution

--- 4.py
+++ 4.py
@@ -1,3 +1,14 @@
+"""
+The only way I arrived to the solution is:
+1) Multipling the two largest numbers with 3 digits (999*999)
+2) Counting down till I find the first palindrome
+3) Finding two factors (if any) less or equal than 999
+"""
+
+"""
+Returns if a number is palindrome by checking 
+head and tail of an array created with that number
+"""
 def is_palindrome(n):
     decomp = [x for x in str(n)]
     first,last = 0, len(decomp)-1
@@ -8,10 +19,25 @@ def is_palindrome(n):
         last -= 1
     return True
 
-def largest_palindrome(x,y):
-    for i in xrange(x,-1, -1):
-        for j in xrange(y,-1,-1):
-            num = i*j
-            if (is_palindrome(num)): return num
+"""
+Finds entire divisors of a numbers. The divisors have to be
+less or equal to the given arguments
+"""
+def find_divisors_less_than(x, lessThan):
+    for i in xrange(lessThan,-1,-1):
+        if (x % i == 0):
+            x = x/i
+            if (x > lessThan): return False
+            return True
+    return False
 
+"""
+Core function generate numbers backwards and check if its 
+a palindrome and find the divisors (if any)
+"""
+def largest_palindrome(x,y):
+    for num in xrange(x*y, -1, -1):
+        if (is_palindrome(num) and find_divisors_less_than(num,x)):
+            return num
+    return False    
 print largest_palindrome(999,999)
